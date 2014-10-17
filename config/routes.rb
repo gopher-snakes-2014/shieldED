512af82acecff5b3e0a9c00c7b86ec906739a5ff
Hiss <anders.e.howerton@gmail.com>
2014-10-17 15:31:07 -0700

    streamlines controllers so routes direct correctly

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
-  get '/admins/dashboard'       => 'users#admin_dashboard',      :as => 'admin_dashboard'
   get '/parents/dashboard'       => 'users#parent_dashboard',      :as => 'parent_dashboard'
   get '/students/dashboard'       => 'users#student_dashboard',      :as => 'student_dashboard'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get '/admins/dashboard'  => 'admins#admin_dashboard'
+  get 'users/admins/dashboard'  => 'admins#admin_dashboard',   :as => 'admin_dashboard'
   # You can have the root of your site routed with "root"
 
 
--- spec/admin_spec/admin_feature_spec.rb
+++ spec/admin_spec/admin_feature_spec.rb
@@ -3,7 +3,7 @@ feature "Admin" do
   scenario 'an admin visits admin page' do
     # @incidents = ["a","b","c","d","e","f","g"] add these when merged with models
     # @incidents.first.date = "2013-19-22"
-    visit "/admins/dashboard"
+    visit "users/admins/dashboard"
     expect(page).to have_content('Report Detail')
     expect(page).to have_content("Recent Reports")
   end
