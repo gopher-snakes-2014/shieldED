f5bd19c528cb6e466511ab6bdcc1296bd8721c22
Hiss <anders.e.howerton@gmail.com>
2014-10-17 11:37:08 -0700

    adds passing rspec. adds style to admin.

--- spec/admin_spec/admin_feature_spec.rb
+++ spec/admin_spec/admin_feature_spec.rb
@@ -2,11 +2,11 @@
 
 feature "Admin" do
   scenario 'an admin visits admin page' do
-    @incidents = ["a","b","c","d","e","f","g"]
-    @incidents.first.date = "2013-19-22"
+    # @incidents = ["a","b","c","d","e","f","g"]
+    # @incidents.first.date = "2013-19-22"
     visit "/admins/dashboard"
 
     expect(page).to have_content('Report Detail')
-    expect(page).to have_content("#{@events.first.date}")
+    expect(page).to have_content("Recent Reports")
   end
 end
