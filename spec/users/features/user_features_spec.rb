ad71782fe4e2a63e6ae3d931f86fd8fc372f4b26
Andrew McClellan <andrew.mcclellan3@gmail.com>
2014-10-17 12:28:56 -0700

    Modify feature tests to ensure Parent/Student do not have Admin content on their dashboard pages Add specificity to test descriptions Add validations for username uniqueness to User model

--- spec/users/features/user_features_spec.rb
+++ spec/users/features/user_features_spec.rb
@@ -1,5 +1,5 @@
 feature 'user management' do
-  scenario "logs a user in based on key code" do
+  scenario "Redirects to admin dashboard page based on admin credentials" do
   Admin1 = User.create(username: "Admin", password_hash: "1234", level: "admin")
     visit root_path
       fill_in "username", with: "Admin"
@@ -8,22 +8,24 @@ feature 'user management' do
     expect(page).to have_content('Admin')
   end
 
-   scenario "logs a user in based on key code" do
+   scenario "Redirects to parent dashboard page based on parent credentials" do
     Parent1 = User.create(username: "Parent", password_hash: "1234", level: "parent")
     visit root_path
       fill_in "username", with: "Parent"
       fill_in "password_hash", with: "1234"
       click_button "login"
     expect(page).to have_content('Parent')
+    expect(page).to_not have_content('Admin')
   end
 
-   scenario "logs a user in based on key code" do
+   scenario "Redirects to student dashboard page based on student credentials" do
     Student1 = User.create(username: "Student", password_hash: "1234", level: "student")
     visit root_path
       fill_in "username", with: "Student"
       fill_in "password_hash", with: "1234"
       click_button "login"
     expect(page).to have_content('Student')
+    expect(page).to_not have_content('Admin')
   end
 
 end
