c0d7cd5acf9e1023864788ca1a6906cff7759734
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-16 19:07:27 -0700

    create first feature test for branch

---
+++ spec/features/submit_observations_feature_spec.rb
@@ -0,0 +1,13 @@
+feature "Events" do
+  scenario "should be submittable from the events/new page" do
+    visit '/events/new'
+    expect(page).to have_content('Record Event')
+
+    within("new_event") do
+    	fill_in "details", with: "I saw mommy kissing santa claus!"
+    	click_button 'Submit'
+    end
+
+    expect(page).to have_content('Thank you!')
+  end
+end
