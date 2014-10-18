8756212d3e5c47ac2cf67cbfa18fab651bde52c5
Hiss <anders.e.howerton@gmail.com>
2014-10-17 21:32:09 -0700

    adds much style to admin dashboard adds email function to admin dashboard adds function to list recent reports adds j-slider adds accurante search form for event details

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,9 +2,14 @@ class AdminsController < ApplicationController
 
   def admin_dashboard
 
-      # @events = Event.all
-      # @total = Event.count
-      # @latest = Event.last(5)
+      @events = Event.all
+      @total = Event.count
+      @latest = Event.last(5)
+  end
 
+  def search
+    @results = Event.search(params[:search])
+    p @results
+    p "*$*$*$*$*$*$*$*$*"
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ActiveRecord::Base
 	validates_presence_of :details
+
+  def self.search(search)
+    if search
+      where("details LIKE ?", ["%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
