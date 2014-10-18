68845016767db4eb92a658a2df2a6b17e63c87ba
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-17 20:55:42 -0700

    add create_event controller test

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -5,4 +5,11 @@ describe EventsController do
   		expect(response).to render_template(:_new)
   	end
   end
+
+  context 'events#create' do
+    it "should create a new event in the database linked to the current user" do
+      @new_event_params = { details: "shit went down in china town" }
+      expect{ post :create, event: @new_event_params }.to change{Event.count}.by(1)
+    end
+  end
 end
