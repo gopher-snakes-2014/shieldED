b25c47f8c7d2c46b060a659c8257725c67a8d34e
Andrew McClellan <andrew.mcclellan3@gmail.com>
2014-10-17 20:07:53 -0700

    Add validations for event_photo to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
 	validates_presence_of :details
+
+  has_attached_file :event_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :event_photo, :content_type => /\Aimage\/.*\Z/
 end
