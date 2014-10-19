d3640ffbc78e4b882b619993626b933d77e49fbd
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-18 19:03:00 -0700

    add epic database seeding code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
 
-school_login = User.create(username: "mySchool", password_hash: "1234", level: "admin")
-Admin1 = User.create(username: "Admin", password_hash: "1234", level: "admin")
-Parent1 = User.create(username: "Parent", password_hash: "1234", level: "parent")
-Student1 = User.create(username: "Student", password_hash: "1234", level: "student")
+User.create(username: "Admin",    password_hash: "1234", level: "admin")
+User.create(username: "Parent",   password_hash: "1234", level: "parent")
+User.create(username: "Student",  password_hash: "1234", level: "student")
+
+500.times do
+  User.create(username: Faker::Internet.user_name, password_hash: "123", level: "student")
+end
+
+145.times do
+  User.create(username: Faker::Internet.user_name, password_hash: "123", level: "parent")
+end
+
+User.all.each do |user|
+  rand(20).times do
+    user.events.create(details:     Faker::Lorem.words(6).join(' '),
+                       offender:    Faker::Name.first_name,
+                       location:    Faker::Address.city,
+                       date:        Faker::Date.between(365.days.ago, 120.days.ago),
+                       created_at:  Faker::Date.between(120.days.ago, Date.today))
+  end
+end
