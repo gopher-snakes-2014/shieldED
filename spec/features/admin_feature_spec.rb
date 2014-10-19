e59134052d09565adf2cc9627da810a0914c07bb
Hiss <anders.e.howerton@gmail.com>
2014-10-19 14:58:11 -0700

    fixes to pass all spec tests

--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -3,7 +3,7 @@ feature "Admin visits dashboard" do
   scenario 'an admin visits admin page' do
     # @incidents = ["a","b","c","d","e","f","g"] add these when merged with models
     # @incidents.first.date = "2013-19-22"
-    visit "users/admins/dashboard"
+    visit "admins/dashboard"
     expect(page).to have_content('Report Detail')
     expect(page).to have_content("Recent Reports")
   end
@@ -11,13 +11,13 @@ end
 
 feature "Admin searches for an incident" do
 
-  scenario 'an admin searches incidents by names of involved parties' do
-    Event.create(submitter: "Jimmy", details: "She said I'm fat.")
-    visit "users/admins/dashboard"
+  scenario 'an admin searches incidents by content of event' do
+    Event.create(submitter: "Jimmy", details: "She said I'm fat.", date: "2011-12-09")
+    visit "admins/dashboard"
 
     fill_in "search", :with => "fat"
     click_button("search")
-    expect(page).to have_content("Jimmy")
+    expect(page).to have_content("2011-12-09")
 
   end
 end
