49b8ffd5760e1f2111b91eabbdc3f6b43106dd6a
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-19 23:39:03 -0700

    pass updated EventTag model test

--- app/models/event_tag.rb
+++ app/models/event_tag.rb
@@ -2,6 +2,8 @@ class EventTag < ActiveRecord::Base
   validates_presence_of :event_id
   validates_presence_of :tag_id
 
+  validates_uniqueness_of :tag_id, scope: :event_id
+
   belongs_to :event
   belongs_to :tag
 end
