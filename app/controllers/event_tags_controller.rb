0516b7eebbda2fcf8575a2e30d7325dfbc9613b6
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-19 23:41:11 -0700

    create event tags controller

---
+++ app/controllers/event_tags_controller.rb
@@ -0,0 +1,14 @@
+class EventTagsController < ApplicationController
+
+  def create
+    @event = Event.find(params[:id])
+
+    params[:tags].each do |tag|
+      @event.event_tags.create(tag_id: tag.to_i)
+    end
+
+    @event = Event.find(params[:id])
+    render '_this_event_tags'
+  end
+
+end
