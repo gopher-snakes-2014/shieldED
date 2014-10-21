4a3d120384eda3280075f107ea169d05a86db262
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-21 12:01:27 -0700

    cleanup console logs and refactor thats with bind

--- app/assets/javascripts/event_tagging.js
+++ app/assets/javascripts/event_tagging.js
@@ -1,5 +1,5 @@
 var TagsModel = function(eventID) {
-  this.receivedTags = "new tags updated from the server"
+  this.receivedTags = "PLACEHOLDER new tags updated from the server"
   this.sendTags = []
   this.eventID = eventID
 }
@@ -7,31 +7,23 @@ var TagsModel = function(eventID) {
 TagsModel.prototype = {
 
   fetch: function() {
-    var self = this
     return $.ajax({
         url: '/events/'+this.eventID+'/event_tags/create',
         type: 'POST',
         data: {tags: this.sendTags},
       })
       .done(function(data) {
-        console.log("here's receivedTags BEFORE being updated: "+self.receivedTags);
-        self.receivedTags = data
-        console.log("here's receivedTags AFTER being updated: "+self.receivedTags);
-        console.log("success");
-      })
+        this.receivedTags = data
+      }.bind(this))
       .fail(function() {
-        console.log("error");
+        console.log("tag fetch fail");
       })
-      .always(function() {
-        console.log("complete");
-      });
   },
 
   collectSelected: function() {
-    var that = this
     $('.selected_tag').each(function(index, el) {
-      that.sendTags.push(el.id)
-    });
+      this.sendTags.push(el.id)
+    }.bind(this));
   }
 
 }
@@ -44,7 +36,6 @@ var TagsView = function(container,availableTags) {
 
 TagsView.prototype = {
   renderNewTags: function(newTags) {
-    console.log("Appending new tags..."+newTags);
     this.container.empty()
     this.container.append(newTags)
   },
@@ -53,10 +44,10 @@ TagsView.prototype = {
     this.availableTags.find('.selected_tag').remove()
   },
 
-  buttonClickMessage: function() {
-    console.log("button got clicked!");
-    console.log("adding new tags for event (id:"+this.eventID+") now...");
-  }
+  toggleTags: function(id){
+      $('#'+id).toggleClass('selected_tag')
+    },
+
 }
 
 var TagsController = function(model,view) {
@@ -67,8 +58,8 @@ var TagsController = function(model,view) {
 TagsController.prototype = {
 
   bindEvents: function() {
-    this.activateAvailableTags()
-    this.activateConfirmButton()
+    $('button').on('click', this.replaceTags.bind(this))
+    this.view.availableTags.on('click', this.toggleSelection.bind(this))
   },
 
   replaceTags: function() {
@@ -77,27 +68,17 @@ TagsController.prototype = {
     $.when(this.model.fetch())
       .then(function() {
         var receivedTags = that.model.receivedTags
-        console.log("Here are the tags I received: "+that.model.receivedTags);
         that.view.renderNewTags(receivedTags);
         that.view.clearUsedTags();
         }
       );
   },
 
-  activateAvailableTags: function() {
-    this.view.availableTags.on('click', function(event) {
-      var tagId = event.target.id
-      $('#'+tagId).toggleClass('selected_tag')
-    });
+  toggleSelection: function(clickEvent){
+    var tagId = clickEvent.target.id
+    console.log("you clicked a tag: "+tagId);
+    this.view.toggleTags(tagId)
   },
-
-  activateConfirmButton: function() {
-    var that = this
-    $('button').on('click', function(event) {
-      that.view.buttonClickMessage()
-      that.replaceTags()
-    });
-  }
 
 }
 
