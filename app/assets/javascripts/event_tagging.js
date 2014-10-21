287c41f2031c7063c3817847a3af8ed76a2bcc69
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-21 00:18:20 -0700

    MVC structure, all but re-appending received tags after ajax

--- app/assets/javascripts/event_tagging.js
+++ app/assets/javascripts/event_tagging.js
@@ -1,34 +1,108 @@
-$('.all_tags').on('click', function(event) {
-  var tagId = event.target.id
-  $('#'+tagId).toggleClass('selected_tag')
-});
+var TagsModel = function(eventID) {
+  this.receivedTags = "new tags updated from the server"
+  this.sendTags = []
+  this.eventID = eventID
+}
 
-tagArray = [];
+TagsModel.prototype = {
 
-$('button').on('click', function(event) {
-  var eventID = $('.event_tags_container')[0].id
-  console.log("adding new tags for event (id:"+eventID+") now...");
+  fetch: function() {
+    var self = this
+    $.ajax({
+        url: '/events/'+this.eventID+'/event_tags/create',
+        type: 'POST',
+        data: {tags: this.sendTags},
+      })
+      .done(function(data) {
+        console.log("here's receivedTags BEFORE being updated: "+self.receivedTags);
+        self.receivedTags = data
+        console.log("here's receivedTags AFTER being updated: "+self.receivedTags);
+        console.log("success");
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      });
+  },
 
-  $('.selected_tag').each(function(index, el) {
-    tagArray.push(el.id)
-  });
+  collectSelected: function() {
+    var that = this
+    $('.selected_tag').each(function(index, el) {
+      that.sendTags.push(el.id)
+    });
+  }
 
-  $.ajax({
-    url: '/events/'+eventID+'/event_tags/create',
-    type: 'POST',
-    data: {tags: tagArray},
-  })
-  .done(function(data) {
-    $('.event_tags_container').empty()
-    $('.event_tags_container').append(data)
-    $('.all_tags').find('.selected_tag').remove()
-    console.log("success");
-  })
-  .fail(function() {
-    console.log("error");
-  })
-  .always(function() {
-    console.log("complete");
-  });
-});
+}
 
+var TagsView = function(container,availableTags) {
+  this.container = container
+  this.eventID = container[0].id
+  this.availableTags = availableTags
+}
+
+TagsView.prototype = {
+  renderNewTags: function(newTags) {
+    console.log("Appending new tags..."+newTags);
+    this.container.empty()
+    this.container.append(newTags)
+  },
+
+  clearUsedTags: function() {
+    this.availableTags.find('.selected_tag').remove()
+  },
+
+  buttonClickMessage: function() {
+    console.log("button got clicked!");
+    console.log("adding new tags for event (id:"+this.eventID+") now...");
+  }
+}
+
+var TagsController = function(model,view) {
+  this.model = model
+  this.view = view
+}
+
+TagsController.prototype = {
+
+  bindEvents: function() {
+    this.activateAvailableTags()
+    this.activateConfirmButton()
+  },
+
+  replaceTags: function() {
+    this.model.collectSelected()
+
+    $.when(this.model.fetch())
+      .then(function() {
+        var receivedTags = this.model.receivedTags
+        console.log("Here are the tags I received: "+this.model.receivedTags);
+        this.view.renderNewTags(receivedTags);
+        this.view.clearUsedTags();
+        }
+      );
+  },
+
+  activateAvailableTags: function() {
+    this.view.availableTags.on('click', function(event) {
+      var tagId = event.target.id
+      $('#'+tagId).toggleClass('selected_tag')
+    });
+  },
+
+  activateConfirmButton: function() {
+    var that = this
+    $('button').on('click', function(event) {
+      that.view.buttonClickMessage()
+      that.replaceTags()
+    });
+  }
+
+}
+
+var view = new TagsView($('.event_tags_container'),$('.all_tags'))
+var model = new TagsModel(view.eventID)
+var controller = new TagsController(model,view)
+
+controller.bindEvents()
